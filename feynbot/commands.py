25d4ec74efdb6e2d906df76600972ca500bd596e
Thomas Cleveland <thomaspcleveland@gmail.com>
2023-09-02 15:41:53 -0700

    + Added command framework, still malfunctional

--- feynbot/commands.py
+++ feynbot/commands.py
@@ -1,20 +1,302 @@
 """The command and command tree classes."""
 
-import discord
-import discord.app_commands as commands
+from asyncio import iscoroutinefunction
+from types import ModuleType
+from typing import Any, Awaitable, Callable, Collection, Iterator, Optional
+from inspect import currentframe
+
+from discord import Object as DObject
+from discord.app_commands import Command as DCommand
+from anytree import NodeMixin, RenderTree
+
+from Feynbot.utility import check_kwargs
+
+# IMPLEMENT: handling parameters, autocomplete, syncing, etc.
 
 
-class CommandTree(commands.CommandTree):
-    """A command tree for Feynbot."""
+class CommandTree(NodeMixin):
+    def __init__(
+        self,
+        node_name: Optional[str] = None,
+        enabled: bool = True,
+        gid: Optional[int] = None,
+        gids: Optional[list[int]] = None,
+        **kwargs: Any,
+    ) -> None:
+        # Pre-Initialization
+        self.override_commands: dict[str, dict[int, Command]] = {}
+        self.global_commands: dict[str, Command] = {}
+        # Construct node
+        super().__init__()
+        # Check for parent
+        parent = kwargs.pop("parent", None)
+        if parent:
+            self.parent_to(parent, inherit=True)
+        # Get file info from where this was defined
+        caller_variables = currentframe().f_back.f_globals  # type: ignore
+        self.origin = kwargs.pop("origin", caller_variables["__name__"])
+        self.file_path = kwargs.pop("file_path", caller_variables["__file__"])
+        # Set attributes
+        self.name = node_name or self.origin
+        self.signature = self.name
+        self.enabled = enabled
 
-    def __init__(self, bot: discord.Client, *args, **kwargs) -> None:
-        """Initialize the command tree."""
-        super().__init__(bot, *args, **kwargs)
+        self.gids: list[int] = list()
+        self.is_global: bool = True
+        if gids and gid:
+            raise ValueError("Cannot have both `gid` and `gids`.")
+        if gid:
+            self.set_gids(gid)
+        elif gids:
+            self.set_gids(*gids)
+        check_kwargs(kwargs)
+
+    # GID Methods
+    # FIX: All of these methods have the potential to break the tree structure since
+    # they don't update whether they store the command globally or as an override.
+    def set_gids(self, *gids: int) -> None:
+        self.gids = list(gids)
+        self.is_global = len(self.gids) == 0
+
+    def add_gids(self, *gids: int) -> None:
+        self.gids.extend([gid for gid in gids if gid not in self.gids])
+        self.is_global = len(self.gids) == 0
+
+    def remove_gids(self, *gids: int) -> None:
+        self.gids = [gid for gid in self.gids if gid not in gids]
+        self.is_global = len(self.gids) == 0
+
+    def get_gid_snowflakes(self) -> tuple[DObject]:
+        snowflakes = []
+        for gid in self.gids:
+            snowflakes.append(DObject(id=gid))
+        return tuple(snowflakes)
+
+    # Logic Methods
+    def has(
+        self,
+        command_name: str,
+        *_,
+        is_global: Optional[bool] = None,
+        gid: Optional[int] = None,
+    ) -> bool:
+        if is_global is None:
+            return self.has(command_name, True) or self.has(command_name, False)
+        if is_global:
+            return command_name in self.global_commands
+        if command_name in self.override_commands:
+            command_set = self.override_commands[command_name]
+            if gid is not None:
+                return gid in command_set
+            return len(command_set) > 0
+        return False
+
+    def parent_to(self, parent: "CommandTree", inherit: bool = True) -> None:
+        self.parent = parent
+        if inherit:
+            self.inherit_from(parent)
+        for command in self.global_commands.values():
+            parent.register_command(command)
+        for command_set in self.override_commands.values():
+            for command in command_set.values():
+                parent.register_command(command)
+
+    def inherit_from(self, node: "CommandTree") -> None:
+        self.enabled = node.enabled
+        self.set_gids(*node.gids)
+
+    def adopt_tree(self, child: "CommandTree", inherit: bool = True) -> None:
+        child.parent_to(self, inherit=inherit)
+
+    # Command Methods
+    def bind(
+        self,
+        command_name: str,
+        node_name: Optional[str] = None,
+        enabled: bool = True,
+        gid: Optional[int] = None,
+        gids: Optional[list[int]] = None,
+        **kwargs: Any,
+    ) -> Callable[[Callable], None]:
+        # Get file info from where this was defined
+        caller_variables = currentframe().f_back.f_globals  # type: ignore
+        origin = kwargs.pop("origin", caller_variables["__name__"])
+        file_path = kwargs.pop("file_path", caller_variables["__file__"])
+
+        command = Command(
+            command_name=command_name,
+            node_name=node_name,
+            enabled=enabled,
+            gid=gid,
+            gids=gids,
+            origin=kwargs.get("origin", origin),
+            file_path=kwargs.get("file_path", file_path),
+            parent=self,
+            **kwargs,
+        )
+        decorator = command.bind(command_name)
+        return decorator
+
+    def register_command(self, command: "Command", recursive: bool = True) -> None:
+        command_name = command.name
+        if command_name is None:
+            raise ValueError(f"Command `{command}` has no name.")
+        # Global command:
+        if command.is_global:
+            self.global_commands[command_name] = command
+        # Override command:
+        if command_name not in self.override_commands:
+            self.override_commands[command_name] = {}
+        command_set = self.override_commands[command_name]
+        for gid in command.gids:
+            # If preexisting:
+            if gid in command_set and command_set[gid] != command:
+                raise ValueError(
+                    f"Command `{command}` conflicts with `{command_set[gid]}`."
+                )
+            command_set[gid] = command
+        if self.parent and recursive:
+            self.parent.register_command(command)
+
+    async def fire(self, name: str, *args, **kwargs) -> None:
+        # IMPLEMENT:  Overrides need to be specially handled here.
+        pass
+
+    # Special Methods
+    def __call__(self, name: str, *args, **kwargs) -> Awaitable[None]:
+        return self.fire(name, *args, **kwargs)
+
+    def __iter__(self) -> Iterator["Command"]:
+        global_commands: Collection[Command] = self.global_commands.values()
+        override_commands: list[Command] = []
+        for command_set in self.override_commands.values():
+            override_commands += command_set.values()
+        all_commands: tuple[Command] = tuple(global_commands) + tuple(override_commands)
+        return iter(all_commands)
+
+    def __str__(self) -> str:
+        string = f"{self.name}"
+        node = self.parent
+        while node:
+            string = f"{node.name}.{string}"
+            node = node.parent
+        return string
+
+    def __repr__(self) -> str:
+        return RenderTree(self).by_attr("signature")
 
 
-class Command(commands.Command):
-    """A command for Feynbot."""
+class Command(CommandTree):
+    def __init__(
+        self,
+        command_name: Optional[str] = None,
+        node_name: Optional[str] = None,
+        enabled: bool = True,
+        gid: Optional[int] = None,
+        gids: Optional[list[int]] = None,  # CHANGE: This can be made `set[int]`
+        **kwargs: Any,
+    ) -> None:
+        # Pre-Initialization
+        self.description: str = kwargs.pop("description", "(No description)")
+        # Get file info from where this was defined
+        caller_variables = currentframe().f_back.f_globals  # type: ignore
+        origin = kwargs.pop("origin", caller_variables["__name__"])
+        file_path = kwargs.pop("file_path", caller_variables["__file__"])
+        # Construct node
+        super().__init__(
+            node_name=node_name,
+            enabled=enabled,
+            gid=gid,
+            gids=gids,
+            origin=kwargs.get("origin", origin),
+            file_path=kwargs.get("file_path", file_path),
+            **kwargs,
+        )
+        # Set attributes
+        self.node_name: Optional[str] = node_name
+        self.signature = self.signature + " $ "
+        self.command_name: Optional[str] = command_name
+        self.function: Optional[Callable[..., Optional[Awaitable[None]]]] = None
+        self.is_coroutine: bool
+        self.discord_command: Optional[DCommand] = None
 
-    def __init__(self, *args, **kwargs) -> None:
-        """Initialize the command."""
-        super().__init__(*args, **kwargs)
+    def bind(
+        self,
+        command_name: str,
+        node_name: Optional[str] = None,
+        enabled: bool = True,
+        gid: Optional[int] = None,
+        gids: Optional[list[int]] = None,
+        **kwargs: Any,  # IMPLEMENT: Overriding attributes/erroring if overriding
+    ) -> Callable[[Callable], None]:
+        # NOTE:  Currently, `file_path` and `origin` are defined by where
+        # the `Command` object itself is defined, not where the bound function is.
+        # This needs to be considered.
+        if self.function:
+            raise ValueError(
+                f"Command `{self.command_name}` already bound for this "
+                f"listener from `{self.origin}`."
+            )
+
+        def decorator(function: Callable[..., Optional[Awaitable[None]]]) -> None:
+            self.function = function
+            if node_name and self.name and self.name != node_name:
+                raise ValueError(
+                    f"Tried passing `node_name = {node_name}` while binding when "
+                    f"`self.name = {self.name}` was already defined in {self}.  "
+                    f"Consider removing one of the two or renaming the function "
+                    f"after binding."
+                )
+            if node_name is None and self.name is None:
+                raise ValueError(
+                    f"Command `{command_name}` from file {self.origin} has no name."
+                )
+            self.name = self.name or node_name
+            self.is_coroutine = iscoroutinefunction(function)
+
+        self.description = kwargs.get("description", self.description)
+        self.command_name = command_name
+        self.signature = self.signature + self.command_name
+        self.register_command(self)
+        return decorator
+
+    def initialized(self) -> bool:
+        if self.function is None:
+            return False
+        if self.command_name is None:
+            return False
+        return True
+
+    async def fire(self, *args, **kwargs) -> None:
+        if not self.enabled:
+            return
+        if not callable(self.function):
+            raise IndexError(
+                f"Command {self.command_name} from file {self.origin} not bound "
+                f"to any valid function but was called."
+            )
+        if iscoroutinefunction(self.function):
+            await self.function(*args, **kwargs)
+        else:
+            self.function(*args, **kwargs)
+
+    def __call__(self, *args: Any, **kwds: Any) -> Awaitable[None]:
+        return self.fire(*args, **kwds)
+
+
+def get_command_trees(
+    module: ModuleType, roots_only: bool = True
+) -> tuple[CommandTree]:
+    variables = dir(module)
+    command_trees = []
+    if "_no_import" in variables:
+        return tuple()
+    for variable in variables:
+        if variable.startswith("__"):
+            continue
+        value = getattr(module, variable)
+        if isinstance(value, CommandTree) and value.parent is None:
+            if roots_only and value.parent is not None:
+                continue
+            command_trees.append(value)
+    return tuple(command_trees)
--- feynbot/handler.py
+++ feynbot/handler.py
@@ -6,7 +6,12 @@ data from the other directories in this package."""
 
 import pathlib
 import importlib
-from typing import Callable
+from typing import Callable, Optional
+from discord import Interaction
+from discord import Object as DObject
+
+import discord.app_commands as app_commands
+from discord.app_commands import Command as DCommand
 
 from Feynbot.commands import CommandTree, get_command_trees
 from Feynbot.events import EventTree, get_event_trees
@@ -22,58 +27,48 @@ class Handler:
         bot,
         events_directory: str,
         commands_directory: str,
-        *args,
-        **kwargs,
     ) -> None:
         self.events_directory = events_directory
         self.commands_directory = commands_directory
         self.bot = bot
         self.console = bot.console
-        self.event_trees: EventTree = EventTree("Bot", inherit=False)
-        self.command_trees: CommandTree = CommandTree("Bot", inherit=False)
-
-        self.load_and_register_all()
-
-        def get_handler(self, event_name) -> Callable:
-            async def handler(*args, **kwargs) -> None:
-                await self.fire_event(event_name, *args, **kwargs)
-
-            handler.__name__ = event_name + "_handler"
-            return handler
-
-        for event in events_list:
-            # TODO: Throw error if an event is enabled but the intent is not.
-            setattr(self, event, get_handler(self, event))
-
-        super().__init__(*args, **kwargs)
-
-    async def fire_event(self, event_name: str, *args, **kwargs) -> None:
-        # TODO: Impement checking, permissions, overrides
-        # TODO: Construct context, pass to event, etc.
-
-        await self.event_trees(
-            event_name, *args, context=None, bot=self.bot, db=None, **kwargs
+        self.event_trees: EventTree = EventTree("Root")
+        self.command_trees: CommandTree = CommandTree("Root")
+        self.dcommand_tree: app_commands.CommandTree = app_commands.CommandTree(
+            bot,
+            fallback_to_global=False,
         )
 
-    # def reload(self) -> None:
-    #     # TODO: Implement with reload command from importlib
+        self.load_and_register_all()
+        self.hook_events()
+        self.hook_commands()
 
+    # General Methods
     def load_and_register_all(self) -> None:
         event_trees = self.load_event_trees()
         for tree in event_trees:
-            tree.parent_to(self.event_trees)
-        command_trees = self.load_commands()
+            tree.parent_to(self.event_trees, inherit=False)
+        command_trees = self.load_command_trees()
         for tree in command_trees:
-            tree.parent_to(self.event_trees)
+            tree.parent_to(self.command_trees, inherit=False)
 
+    def reload_all(self) -> None:
+        self.reload_events()
+        self.reload_commands()
+
+    def reload_events(self) -> None:
+        pass
+
+    def reload_commands(self) -> None:
+        pass
+
+    # Event Methods
     def load_event_trees(self) -> tuple[EventTree]:
         all_trees = []
         for path in pathlib.Path(self.events_directory).rglob("*.py"):
             if not path.is_file():
                 continue
             module = import_from_path(str(path))
-            if module.no_import is True:
-                continue
             trees = get_event_trees(module)
             if len(trees) == 0:
                 self.bot.warn(f"Module `{module}` has no event trees.")
@@ -81,17 +76,90 @@ class Handler:
             all_trees += trees
         return tuple(all_trees)
 
+    def hook_events(self) -> None:
+        def hook(self, event_name) -> Callable:
+            async def handler(*args, **kwargs) -> None:
+                await self.fire_event(event_name, *args, **kwargs)
+
+            handler.__name__ = event_name + "_event_hook"
+            return handler
+
+        for event in events_list:
+            # TODO: Throw error if an event is enabled but the intent is not.
+            setattr(self, event, hook(self, event))
+        return
+
+    async def fire_event(self, event_name: str, *args, **kwargs) -> None:
+        # TODO: Impement checking, permissions, overrides
+        # TODO: Construct context, pass to event, etc.
+        raw = {
+            "args": args,
+            **kwargs,
+        }
+        kwargs = {
+            "context": None,
+            "bot": self.bot,
+            "db": None,
+            "raw": raw,
+        }
+        await self.event_trees(event_name, **kwargs)
+        return
+
+    # Command Methods
     def load_command_trees(self) -> tuple[CommandTree]:
         all_trees = []
         for path in pathlib.Path(self.commands_directory).rglob("*.py"):
-            if not path.is_file():
-                continue
             module = import_from_path(str(path))
-            if module.no_import is True:
-                continue
             trees = get_command_trees(module)
             if len(trees) == 0:
                 self.bot.warn(f"Module `{module}` has no command trees.")
                 continue
             all_trees += trees
         return tuple(all_trees)
+
+    def hook_commands(self) -> None:
+        def hook(self, command_name) -> Callable:
+            async def handler(interaction: Interaction) -> None:
+                # NOTE: Will need to pass Interaction or make a Context
+                self.log(f"Command `{command_name}` fired.")
+                await self.fire_command(command_name)
+
+            handler.__name__ = command_name + "_command_hook"
+            return handler
+
+        print(repr(self.command_trees))
+        for command in self.command_trees:
+            # FIX: Currently, there are subtle errors, such as when description
+            # is an empty string.  These need to raise an exception.
+            if command.initialized() is False:
+                raise ValueError(f"Command `{command.name}` is not initialized.")
+            self.dcommand_tree.command(
+                name=command.command_name,
+                description=command.description,
+                guilds=command.get_gid_snowflakes(),
+            )(hook(self, command.name))
+        return
+
+    async def sync_commands(self, *guilds: Optional[DObject]) -> None:
+        if len(guilds) == 0:
+            await self.dcommand_tree.sync()
+            return
+        for guild in guilds:
+            await self.dcommand_tree.sync(guild=guild)
+        return
+
+    async def fire_command(self, command_name: str, *args, **kwargs) -> None:
+        # TODO: Impement checking, permissions, overrides
+        # TODO: Construct context, pass to event, etc.
+        raw = {
+            "args": args,
+            **kwargs,
+        }
+        kwargs = {
+            "context": None,
+            "bot": self.bot,
+            "db": None,
+            "raw": raw,
+        }
+        await self.command_trees(command_name, **kwargs)
+        return
